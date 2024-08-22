3f7b0dd7498dfe67a4b351fc121c7d1e3140ae80
EnciJeffrey13 <ashleyjeffrey2011@gmail.com>
2024-08-22 09:57:29 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -28,14 +28,14 @@
 
 char size(int cms) {
     char sizeName = '\0';
-    if (cms < 38) {
-        return 'S';
-    } else if (cms <= 42) {  // Adjusted to include 38-42 as 'M'
-        return 'M';
-    } else {
-        return 'L';
+    if(cms < 38) {
+        sizeName = 'S';
+    } else if(cms > 38 && cms < 42) {
+        sizeName = 'M';
+    } else if(cms > 42) {
+        sizeName = 'L';
     }
-    return sizeName
+    return sizeName;
 }
 
 int main() {
